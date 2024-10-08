64f9b2000de215d181dfab779b7eae45727579a9
TontonJaj <raphael.dufour7@hotmail.com>
2024-10-08 17:33:41 -0400

    Bug fix
    Now when block touch selling counter, it should disapear and not crash everything even tho the player still holding the wood

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -9,19 +9,19 @@ var XP = 0
 var picked = false
 	#log value # Replace with function body.
 		
-	
-
-
-
 
 #selling wood if wood is wood
 func _on_sell_button_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
-	var player = $Player
-	var collider = $AdventurerGuildCounter/MeshInstance3D/SellButton/CollisionShape3D/SellZone
-	var collided_item = collider.get_collider()
-	if collided_item != null and collided_item is RigidBody3D:
-		if collided_item.is_in_group("trees"):
-			picked = false
-			collided_item.queue_free()
+	print("has entered")
+	
+	#trying to take size of the wood and * it by value of wood type
+	var bodyvalue = body.get("MeshInstanc3D/Size")
+	print(bodyvalue)
+	
+	if body.is_in_group("trees") and body is RigidBody3D:
+		Global.picked = false
+		var body_value = body
+		body.queue_free()
+			
 	else:
-		print("is not a tree")
+		print("is missing a condition (ingrouptree/rigidbody3d)")
--- Scripts/treetier1.gd
+++ Scripts/treetier1.gd
@@ -1,6 +1,1 @@
 extends RigidBody3D
-
-
-func _input(event):
-	var tree = $"."
-	tree.add_to_group("trees")
