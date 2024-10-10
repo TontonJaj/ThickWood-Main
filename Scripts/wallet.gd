116155cdd819cb4c31d545c17c85946eaf9a6a8d
TontonJaj <raphael.dufour7@hotmail.com>
2024-10-09 23:18:24 -0400

    fixed ready function so that the coins initialize when start

--- Scripts/wallet.gd
+++ Scripts/wallet.gd
@@ -6,9 +6,7 @@ var SP : int = 0
 var GP : int = 0
 var PP : int = 0
 
-func _on_ready():
-	#why no nice ? i dont get it its fucked up maybe onready dont work when in wallet ? anyway me tired i did a thing so good night
-	print("nice")
+func _ready():
 	update_money_wallet()
 	
 		
