30d6681cd1a856de71a6ec40e341011e1ed4bc6f
pizzaenfeu <145949794+pizzaenfeu@users.noreply.github.com>
2024-10-17 00:56:39 -0400

    Meshing visuals
    Did the whole thing again, less buggy, more straightforward to my intentions, clearer use of functions. Still need to understand and add vertices at the collision points from the axe but at least the result is going to be obvious when implementing.

--- Scripts/axe.gd
+++ Scripts/axe.gd
@@ -58,7 +58,7 @@ func collision_all_raycasts_on_tree() -> void:
 		handle_tree_collision()
 		return
 	for raycast in raycasts.get_children():
-		if raycast is RayCast3D and raycast.is_colliding() and raycast.get_collider().name == "DeadTreeStatic":
+		if raycast is RayCast3D and raycast.is_colliding() and raycast.get_collider().name == "DeadTreeStatic": #need another condition for multiple trees
 			if !collidedTreeDictionnary.collider:
 				collidedTreeDictionnary.collider = raycast.get_collider()
 			if raycast.name == "RayCast3DTop" and !collidedTreeDictionnary.top:
@@ -71,21 +71,36 @@ func collision_all_raycasts_on_tree() -> void:
 func handle_tree_collision() -> void:
 	choppedAlready = true
 	print('madeittocollision')
-	if collidedTreeDictionnary.top:
-		visualize_collision_point(collidedTreeDictionnary.top)
-	if collidedTreeDictionnary.depth:
-		visualize_collision_point(collidedTreeDictionnary.depth)
-	if collidedTreeDictionnary.bottom:
-		visualize_collision_point(collidedTreeDictionnary.bottom)
-	collidedTreeDictionnary.collider.find_parent("Trees").add_vertices_to_tree_mesh(collidedTreeDictionnary)
+	#if collidedTreeDictionnary.top:
+		#visualize_collision_point(collidedTreeDictionnary.top)
+	#if collidedTreeDictionnary.depth:
+		#visualize_collision_point(collidedTreeDictionnary.depth)
+	#if collidedTreeDictionnary.bottom:
+		#visualize_collision_point(collidedTreeDictionnary.bottom)
+	collidedTreeDictionnary.collider.find_parent("Trees").handleChop(collidedTreeDictionnary)
 	
 
 func visualize_collision_point(collisionPos: Vector3) -> void:
+	if not is_inside_tree():
+		print("Warning: Attempting to visualize collision point while not in scene tree.")
+		return
+
 	var sphere = CSGSphere3D.new()
 	sphere.name = "Node_" + str(Time.get_ticks_msec())
-	sphere.radius = 0.01
-	sphere.global_position = collisionPos
-	get_tree().root.add_child(sphere)
+	sphere.radius = 0.015
+
+	# Create a new material
+	var material = StandardMaterial3D.new()
+	material.albedo_color = Color.WEB_PURPLE
+	
+	# Assign the material to the sphere
+	sphere.material = material
+
+	# Add the sphere to the scene first
+	get_tree().current_scene.add_child(sphere)
+
+	# Then set its position
+	sphere.global_transform.origin = collisionPos
 
 func create_cpuparticles_3d()->void:
 	pass
